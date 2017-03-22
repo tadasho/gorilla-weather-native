23cf5f6ba51cb707f28e0b647b13e25205d33f23
ShoTada <shownfy05@gmail.com>
2017-03-22 22:28:24 +0900

    [add] お天気情報を表示する

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * gorilla-weather-native
+ * https://github.com/tadasho/gorilla-weather-native
  */
-
+'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,6 +11,7 @@ import {
   View,
   Navigator,
   TouchableHighlight,
+  Image,
 } from 'react-native';
 import {
   Container,
@@ -27,6 +27,7 @@ import {
   Icon,
   Card,
   CardItem,
+  Thumbnail,
 } from 'native-base';
 import {
   StackNavigator,
@@ -37,15 +38,58 @@ import {
   MyOtherRoutes,
 } from 'react-navigation';
 
+var REQUEST_URL = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=280010';
+
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      site: "",
+    };
+  }
   static navigationOptions = {
     title: 'Welcome',
     header: () => ({
       visible: false,
     })
   };
+  fetchData() {
+    fetch(REQUEST_URL, {
+      method: 'get',
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          site: responseData,
+        });
+      })
+      .done();
+  }
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading...
+					</Text>
+      </View>
+    );
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
     const { navigate } = this.props.navigation;
+    var site = this.state.site;
+    const forecast = site.forecasts;
+    if (!site) {
+      return this.renderLoadingView();
+    }
     return (
       <Container>
         <Header>
@@ -59,28 +103,51 @@ class HomeScreen extends React.Component {
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title>{site.location.city}の天気</Title>
           </Body>
           <Right />
         </Header>
 
         <Content>
-          <Card>
-            <CardItem header>
-              <Text>NativeBase</Text>
-            </CardItem>
 
+          <Card>
             <CardItem>
               <Body>
-                <Text>
-                  Hello!
-                </Text>
+                <Text>{site.description.text}</Text>
               </Body>
             </CardItem>
-            <CardItem footer>
-              <Text>GeekyAnts</Text>
-            </CardItem>
           </Card>
+
+          <View>
+            {forecast.map((tenki) =>
+
+              <Card key={tenki.date}>
+                <CardItem bordered>
+                  <Left>
+                    <Image source={{ uri: tenki.image.url }}
+                      style={{ width: tenki.image.width, height: tenki.image.height }} />
+                    <Body>
+                      <Text>{tenki.dateLabel}の天気</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem header>
+                  <Text>{tenki.telop}</Text>
+                </CardItem>
+                <CardItem>
+                  <Text>
+                    最高気温: {tenki.temperature.max == null
+                      ? "--"
+                      : tenki.temperature.max.celsius}
+                    {'\n'}
+                    最低気温: {tenki.temperature.min == null
+                      ? "--"
+                      : tenki.temperature.min.celsius}</Text>
+                </CardItem>
+              </Card>
+
+            )}
+          </View>
         </Content>
       </Container>
     );
@@ -104,7 +171,16 @@ const SimpleApp = StackNavigator({
   Home: { screen: HomeScreen },
   Setting: { screen: SettingScreen },
 }, {
-  headerMode: 'screen',
-});
+    headerMode: 'screen',
+  });
 
 export default SimpleApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gorilla-weather-native
+
+### memo
+fix：バグ修正
+add：新規（ファイル）機能追加
+update：機能修正（バグではない）
+remove：削除（ファイル）
